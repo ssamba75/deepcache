547ee9b6a0fd9d6d528fe87ea9404787e13d1c4e
이준호 <ssamba75@tmon.co.kr>
2016-05-09 12:19:46 +0900

    1. replace FunctionInterface param with Function. 2. library version up. 3. From now, Caching List<K> value is Available!

--- src/main/java/com/juno/deepcache/core/DeepCache.java
+++ src/main/java/com/juno/deepcache/core/DeepCache.java
@@ -9,35 +9,34 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class DeepCache {
 
 	public static final String PREFIX_DEEP_CACHE = "deepCache_";
 
-	public static <K, V> Map<K, V> cacheable(Cache cache, String elementName, List<K> keys, FunctionInterface<List<K>, Map<K, V>> function) {
+	/**
+	 * Cacheable map.
+	 *
+	 * @param <K>         the type parameter
+	 * @param <V>         the type parameter
+	 * @param cache       the cache
+	 * @param elementName the element name
+	 * @param keys        the keys
+	 * @param apiMethod   the api method
+	 * @return the map
+	 */
+	public static <K, V> Map<K, V> cacheable(Cache cache, String elementName, List<K> keys, Function<List<K>, Map<K, V>> apiMethod) {
 
 		try {
 
-			List<K> elementKeysIsNotCachedYet = keys.stream()
-					.map(dealNo -> {
-						if (getElementFromEhCache(cache, elementName, dealNo) == null) {
-							return dealNo;
-						} else {
-							return null;
-						}
-					})
-					.filter(Objects::nonNull)
-					.collect(Collectors.toList());
+			List<K> elementKeysIsNotCachedYet = getKeysIsNotCachedYet(cache, elementName, keys);
 
 			if (!elementKeysIsNotCachedYet.isEmpty()) {
 
-				Optional<Map<K, V>> apiResult = Optional.ofNullable(function.getResult(elementKeysIsNotCachedYet));
-				if (apiResult.isPresent() == false) {
-					return null;
-				}
-
-				if (!apiResult.get().isEmpty()) {
+				Optional<Map<K, V>> apiResult = Optional.ofNullable(apiMethod.apply(elementKeysIsNotCachedYet));
+				if (apiResult.isPresent() && !apiResult.get().isEmpty()) {
 					apiResult.get().forEach((k, v) -> setElementOnEhCache(cache, elementName, k, v));
 				}
 			}
@@ -47,24 +46,69 @@ public class DeepCache {
 			return cachedElementMap;
 
 		} catch (Exception e) {
-			e.printStackTrace();
 			return Collections.emptyMap();
 		}
+	}
 
+	private static <K> List<K> getKeysIsNotCachedYet(Cache cache, String elementName, List<K> keys) {
+
+		return keys.stream()
+				.filter(key -> getElementFromEhCache(cache, elementName, key).isPresent() == false)
+				.collect(Collectors.toList());
+	}
+
+	/**
+	 * Cacheable map.
+	 *
+	 * @param <K>         the type parameter
+	 * @param <V>         the type parameter
+	 * @param cache       the cache
+	 * @param elementName the element name
+	 * @param keys        the keys
+	 * @param apiMethod   the api method
+	 * @param keyMapper   the key mapper
+	 * @return the map
+	 */
+	public static <K, V> List<V> cacheable(Cache cache, String elementName, List<K> keys, Function<List<K>, List<V>> apiMethod, Function<V, K> keyMapper) {
+
+		try {
+
+			List<K> elementKeysIsNotCachedYet = getKeysIsNotCachedYet(cache, elementName, keys);
+
+			if (!elementKeysIsNotCachedYet.isEmpty()) {
+
+				Optional<List<V>> apiResult = Optional.ofNullable(apiMethod.apply(elementKeysIsNotCachedYet));
+				if (apiResult.isPresent()) {
+					Map<K, V> mappedApiResult = apiResult.get().stream().collect(Collectors.toMap(keyMapper, Function.identity()));
+
+					if (!mappedApiResult.isEmpty()) {
+						mappedApiResult.forEach((k, v) -> setElementOnEhCache(cache, elementName, k, v));
+					}
+				}
+			}
+
+			return keys.stream()
+					.map(key -> (V) getElementFromEhCache(cache, elementName, key).orElse(null))
+					.filter(Objects::nonNull)
+					.collect(Collectors.toList());
+
+		} catch (Exception e) {
+			return Collections.emptyList();
+		}
 	}
 
 	private static <K, V> Optional<V> getElementFromEhCache(Cache cache, String elementName, K key) {
 
-		Optional<Element> cacheElement = Optional.ofNullable(cache.get(PREFIX_DEEP_CACHE + elementName + key.hashCode()));
-		if (cacheElement.isPresent()) {
-			return Optional.ofNullable((V) cacheElement.get().getObjectValue());
+		Element cacheElement = cache.get(PREFIX_DEEP_CACHE + elementName + key.hashCode());
+		if (Objects.nonNull(cacheElement)) {
+			return Optional.ofNullable((V) cacheElement.getObjectValue());
 		} else {
-			return null;
+			return Optional.ofNullable(null);
 		}
 	}
 
 	private static <K, V> void setElementOnEhCache(Cache cache, String elementName, K key, V value) {
+
 		cache.put(new Element(PREFIX_DEEP_CACHE + elementName + key.hashCode(), value));
 	}
-
 }
--- src/main/java/com/juno/deepcache/core/FunctionInterface.java
+++
@@ -1,9 +0,0 @@
-package com.juno.deepcache.core;
-
-import java.util.List;
-import java.util.Map;
-
-public interface FunctionInterface<K, V> {
-
-	public <K, V> Map<K, V> getResult(List<K> keys);
-}
